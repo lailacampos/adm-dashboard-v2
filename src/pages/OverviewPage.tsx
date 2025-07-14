5da6af83806bbfd4526c6332926efca12d276d2f
Laila <lailarhcampos@gmail.com>
2025-07-13 23:26:59 -0300

    Add NotFoundPage and OverviewPage components with basic layout

---
+++ src/pages/OverviewPage.tsx
@@ -0,0 +1,12 @@
+
+
+const OverviewPage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4">Overview Page</h1>
+      <p className="text-lg text-gray-700">This is the overview page content.</p>
+    </div>
+  );
+}
+
+export default OverviewPage;
